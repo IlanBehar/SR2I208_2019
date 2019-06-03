d5eda1a0077ee9600ae3f6cb8d0c4034320fd1fd
ilan <ilan.behar@telecom-paristech.fr>
2019-06-03 17:16:18 +0200

    WIP

---
+++ GetCarsPosition.m
@@ -0,0 +1,6 @@
+function [output] = GetCarsPosition(carPerSecond, laneNumber, speed, roadLength)
+%Set the number of car on the road
+carsPerLanePerSecond=carPerSecond/laneNumber;
+cars=[1:speed/carsPerLanePerSecond:roadLength];
+output=cars
+
--- highway.m
+++ highway.m
@@ -1,8 +1,8 @@
 %%Initiate a highway
 laneWidth=2;
 laneNumber=3;
-roadLength=10000;
-
+roadLength=100;
+speed=100/3.6;
 s = drivingScenario;
 
 
@@ -10,4 +10,14 @@ roadCenters = [0 0; roadLength 0];
 
 road(s, roadCenters, 'lanes', lanespec([laneNumber laneNumber]));
 
+position=GetCarsPosition(5, laneNumber,speed ,roadLength);
+y_pos=(-10:20/6:10);
+i=1;
+for pos=position
+    y=y_pos(i);
+    v = vehicle(s,'Position',[ pos y 0],'Velocity',[ 0 speed 0],'Yaw',0);
+    i=mod(i,laneNumber*2)+1;
+end
+
 plot(s,'RoadCenters','on');
+
