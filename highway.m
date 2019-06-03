3f0e0a581d271d75a778308db09b16b4d375d84a
ilan <ilan.behar@telecom-paristech.fr>
2019-06-03 16:01:52 +0200

    added a simple highway road

---
+++ highway.m
@@ -0,0 +1,13 @@
+%%Initiate a highway
+laneWidth=2;
+laneNumber=3;
+roadLength=100;
+
+s = drivingScenario;
+
+
+roadCenters = [0 0; roadLength 0];
+
+road(s, roadCenters, 'lanes', lanespec([laneNumber laneNumber]));
+
+plot(s,'RoadCenters','on');
