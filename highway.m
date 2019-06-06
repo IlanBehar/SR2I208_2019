e8617be2d0e05108148c87691d2c87ef15f7f0ec
ilan <ilan.behar@telecom-paristech.fr>
2019-06-06 15:07:57 +0200

    added a fix axis and speed variation

--- highway.m
+++ highway.m
@@ -1,8 +1,9 @@
 %%Initiate a highway
 laneWidth=3.6;
 laneNumber=3;
-roadLength=100;
-speed=100/3.6;
+roadLength=1000;
+speed=100/3.6;%km/h -> m/s
+speedRange=floor(60/3.6); %speed can go from speed-speedRange/2 to speed+speedRange/2
 s = drivingScenario;
 
 
@@ -28,15 +29,18 @@ line=[line1 line2 line3 line4 line5 line6];
 
 for pos=position
     currentLine=line(1:2,i*2-1:i*2);
-    v = vehicle(s,'Position',[ pos currentLine(1) 0],'Velocity',[ 0 speed 0],'Yaw',0);
+    speedVar=randi(speedRange)-speedRange/2;
+    mySpeed=speed+speedVar;
+    v = vehicle(s,'Position',[ pos currentLine(1) 0],'Velocity',[ 0 0 0],'Yaw',0);
     i=mod(i,laneNumber*2)+1;
-    traj=[pos currentLine(1); 1000 currentLine(1)];
-    trajectory(v,traj(:,:), 30);
+    traj=[pos currentLine(1); roadLength*2 currentLine(1)];
+    trajectory(v,traj(:,:), mySpeed);
 end
 
 plot(s,'RoadCenters','on');
-s.SampleTime = 0.02
-s.StopTime = 4;
+s.SampleTime = 0.02;
+s.StopTime = 10;
 while advance(s)
+  axis([roadLength*1/3 roadLength*2/3 -3*laneWidth 3*laneWidth])
   pause(0.001)
 end
