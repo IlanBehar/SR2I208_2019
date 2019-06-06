f534281a78efe200a4cfbcd5faa765495496793e
ilan <ilan.behar@telecom-paristech.fr>
2019-06-06 15:30:05 +0200

    added the generation of lines in a loop to automate it

--- highway.m
+++ highway.m
@@ -1,32 +1,43 @@
 %%Initiate a highway
-laneWidth=3.6;
+
+%Data to set
 laneNumber=3;
 roadLength=1000;
 speed=100/3.6;%km/h -> m/s
 speedRange=floor(60/3.6); %speed can go from speed-speedRange/2 to speed+speedRange/2
+
+
+
+laneWidth=3.6;
 s = drivingScenario;
-
-
 roadCenters = [0 0; roadLength 0];
-
 road(s, roadCenters, 'lanes', lanespec([laneNumber laneNumber]));
-
 position=GetCarsPosition(5, laneNumber,speed ,roadLength);
-i=1;
 
 d = 3.6/2;
-h = .45/2;
 roadOffset = [ d 0; d 0]';
-lane=[laneWidth 0 ; 0 laneWidth];        
-line1 = roadCenters - roadOffset - 2*lane
+lane=[laneWidth 0 ; 0 laneWidth];  
+line=[];
+
+for i=-laneNumber: laneNumber-1
+    %i=index-laneNumber
+    if i>=0
+        currentLine=roadCenters + roadOffset + i*lane;
+    else
+        currentLine=roadCenters - roadOffset + (i+1)*lane;
+    end
+    line=[line currentLine];
+end
+line1 = roadCenters - roadOffset - 2*lane;
 line2 = roadCenters - roadOffset - lane;
 line3 = roadCenters - roadOffset;
 line4 = roadCenters + roadOffset;
 line5 = roadCenters + roadOffset +lane;
 line6 = roadCenters + roadOffset +2*lane;
-line=[line1 line2 line3 line4 line5 line6];
-% loop around the track four times
+%line=[line1 line2 line3 line4 line5 line6];
 
+
+i=1;
 for pos=position
     direction=1;
     currentLine=line(1:2,i*2-1:i*2);
@@ -43,8 +54,8 @@ end
 
 plot(s,'RoadCenters','on');
 s.SampleTime = 0.02;
-s.StopTime = 10;
+s.StopTime = 2;
 while advance(s)
-  axis([roadLength*1/3 roadLength*2/3 -3*laneWidth 3*laneWidth])
-  pause(0.001)
+  axis([roadLength*1/3 roadLength*2/3 -3*laneWidth 3*laneWidth]);
+  pause(0.001);
 end
