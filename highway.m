9db5835fd2321eb6fe6a4be3b8c018ee2f2bdccd
ilan <ilan.behar@telecom-paristech.fr>
2019-06-06 14:13:19 +0200

    positionning cars correctly in regard to the y line

--- highway.m
+++ highway.m
@@ -1,5 +1,5 @@
 %%Initiate a highway
-laneWidth=2;
+laneWidth=3.6;
 laneNumber=3;
 roadLength=100;
 speed=100/3.6;
@@ -11,13 +11,32 @@ roadCenters = [0 0; roadLength 0];
 road(s, roadCenters, 'lanes', lanespec([laneNumber laneNumber]));
 
 position=GetCarsPosition(5, laneNumber,speed ,roadLength);
-y_pos=(-10:20/6:10);
 i=1;
+
+d = 3.6/2;
+h = .45/2;
+roadOffset = [ d 0; d 0]';
+       
+line1 = roadCenters - roadOffset - 2*laneWidth;
+line2 = roadCenters - roadOffset - laneWidth
+line3 = roadCenters - roadOffset
+line4 = roadCenters + roadOffset;
+line5 = roadCenters + roadOffset +laneWidth;
+line6 = roadCenters + roadOffset +2*laneWidth;
+line=[line1 line2 line3 line4 line5 line6];
+% loop around the track four times
+
 for pos=position
-    y=y_pos(i);
-    v = vehicle(s,'Position',[ pos y 0],'Velocity',[ 0 speed 0],'Yaw',0);
+    currentLine=line(1:2,i*2-1:i*2);
+    currentLine(1)
+    v = vehicle(s,'Position',[ pos currentLine(1) 0],'Velocity',[ 0 speed 0],'Yaw',0);
     i=mod(i,laneNumber*2)+1;
+    %trajectory(v,currentLine(:,:), 30);
 end
 
 plot(s,'RoadCenters','on');
-
+%s.SampleTime = 0.02
+%s.StopTime = 4;
+%while advance(s)
+%  pause(0.001)
+%end
