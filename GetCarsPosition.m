3874b58e440330d827795c39b316b54d55d95b56
ilan <ilan.behar@telecom-paristech.fr>
2019-06-06 14:12:47 +0200

    added a semi-colon to prevent result from being displayed

--- GetCarsPosition.m
+++ GetCarsPosition.m
@@ -2,5 +2,5 @@ function [output] = GetCarsPosition(carPerSecond, laneNumber, speed, roadLength)
 %Set the number of car on the road
 carsPerLanePerSecond=carPerSecond/laneNumber;
 cars=[1:speed/carsPerLanePerSecond:roadLength];
-output=cars
+output=cars;
 
