52847bb049bba6ed886473ee8bbdd2f7a5b3accc
ilan <ilan.behar@telecom-paristech.fr>
2019-06-06 14:28:00 +0200

    added the trajectory but issue with the side effects

--- highway.m
+++ highway.m
@@ -16,27 +16,27 @@ i=1;
 d = 3.6/2;
 h = .45/2;
 roadOffset = [ d 0; d 0]';
-       
-line1 = roadCenters - roadOffset - 2*laneWidth;
-line2 = roadCenters - roadOffset - laneWidth
-line3 = roadCenters - roadOffset
+lane=[laneWidth 0 ; 0 laneWidth];        
+line1 = roadCenters - roadOffset - 2*lane
+line2 = roadCenters - roadOffset - lane;
+line3 = roadCenters - roadOffset;
 line4 = roadCenters + roadOffset;
-line5 = roadCenters + roadOffset +laneWidth;
-line6 = roadCenters + roadOffset +2*laneWidth;
+line5 = roadCenters + roadOffset +lane;
+line6 = roadCenters + roadOffset +2*lane;
 line=[line1 line2 line3 line4 line5 line6];
 % loop around the track four times
 
 for pos=position
     currentLine=line(1:2,i*2-1:i*2);
-    currentLine(1)
     v = vehicle(s,'Position',[ pos currentLine(1) 0],'Velocity',[ 0 speed 0],'Yaw',0);
     i=mod(i,laneNumber*2)+1;
-    %trajectory(v,currentLine(:,:), 30);
+    traj=[pos currentLine(1); 1000 currentLine(1)];
+    trajectory(v,traj(:,:), 30);
 end
 
 plot(s,'RoadCenters','on');
-%s.SampleTime = 0.02
-%s.StopTime = 4;
-%while advance(s)
-%  pause(0.001)
-%end
+s.SampleTime = 0.02
+s.StopTime = 4;
+while advance(s)
+  pause(0.001)
+end
