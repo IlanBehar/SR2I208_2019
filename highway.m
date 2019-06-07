234c03fdb0bb92840425d6c4fb249412cca42869
ilan <ilan.behar@telecom-paristech.fr>
2019-06-07 10:29:27 +0200

    changed layout of a matrix for more consistency

--- highway.m
+++ highway.m
@@ -2,7 +2,7 @@
 
 %Data to set
 laneNumber=3;
-roadLength=1000;%in meters
+roadLength=100;%in meters
 speed=100/3.6;%km/h -> m/s
 speedRange=floor(60/3.6); %speed can go from speed-speedRange/2 to speed+speedRange/2
 carsPerSecond=5;
@@ -16,12 +16,12 @@ position=GetCarsPosition(carsPerSecond, laneNumber,speed ,roadLength);
 
 d = laneWidth/2;
 roadOffset = [ d 0; d 0]';
-lane=[laneWidth 0 ; 0 laneWidth];  
+lane=[laneWidth laneWidth ; 0 0];  
 line=[];
 
 for i=-laneNumber: laneNumber-1
     if i>=0
-        currentLine=roadCenters + roadOffset + i*lane
+        currentLine=roadCenters + roadOffset + i*lane;
     else
         currentLine=roadCenters - roadOffset + (i+1)*lane;
     end
@@ -39,7 +39,7 @@ for pos=position
         direction=-1;
     end
     v = vehicle(s,'Position',[ pos currentLine(1) 0],'Velocity',[ 0 0 0],'Yaw',0);
-    traj=[pos currentLine(1); direction*roadLength*2 currentLine(1)];
+    traj=[pos currentLine(1); direction*roadLength*2 currentLine(1)]; %*2 to allow more time for the simulation
     trajectory(v,traj(:,:), mySpeed);
     i=mod(i,laneNumber*2)+1;
 end
@@ -48,6 +48,6 @@ plot(s,'RoadCenters','on');
 s.SampleTime = 0.01;
 s.StopTime = 2;
 while advance(s)
-  axis([roadLength*1/3 roadLength*2/3 -3*laneWidth 3*laneWidth]);
+  %axis([roadLength*1/3 roadLength*2/3 -3*laneWidth 3*laneWidth]);
   pause(0.001);
 end
