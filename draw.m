7b39c0e49a009406a68243e5a495706689793d57
ilan <ilan.behar@telecom-paristech.fr>
2019-06-10 15:24:42 +0200

    added the plot of one sensor but doesnt do anything yet

--- draw.m
+++ draw.m
@@ -4,13 +4,13 @@ function [] = draw(s, target, attack)
 hFigure = figure;
 hFigure.Position(3) = 900;
 %% Scenario plot
-hPanel1 = uipanel(hFigure,'Units','Normalized','Position',[0 0 1/2 1],'Title','Scenario Plot');
+hPanel1 = uipanel(hFigure,'Units','Normalized','Position',[0 0 1/3 1],'Title','Scenario Plot');
 hAxes1 = axes('Parent',hPanel1);
 plot(s,'RoadCenters','on','Parent',hAxes1);
 
 
 %% Track plot
-hPanel2 = uipanel(hFigure,'Units','Normalized','Position',[1/2 0 1/2 1],'Title','Track Plot');
+hPanel2 = uipanel(hFigure,'Units','Normalized','Position',[1/3 0 1/3 1],'Title','Track Plot');
 hAxes2 = axes('Parent',hPanel2);
 carBEP = birdsEyePlot('Parent',hAxes2, 'Xlimits', [-50 50], 'Ylimits', [-20 20]);
 targetLanePlotter = laneBoundaryPlotter(carBEP);
@@ -21,7 +21,28 @@ plotTrack(targetTrackPlotter, [0 0]);
 
 
 %% Sensor plot
+hPanel3 = uipanel(hFigure,'Units','Normalized','Position',[2/3 0 1/3 1],'Title','Sensor Plot');
+hAxes3 = axes('Parent',hPanel3);
+sensorBEP = birdsEyePlot('Parent',hAxes3,'Xlimits', [0 90], 'Ylimits', [-35 35]);
+cap(1) = coverageAreaPlotter(sensorBEP,'FaceColor','blue','EdgeColor','blue');
+%cap(2) = coverageAreaPlotter(sensorBEP,'FaceColor','red','EdgeColor','red');
+%cap(3) = coverageAreaPlotter(sensorBEP,'FaceColor','red','EdgeColor','red');
 
+radarSensor = radarDetectionGenerator(...
+    'SensorIndex', 1, ...
+    'SensorLocation', [target.Wheelbase+target.FrontOverhang 0], ...
+    'Height', 0.2, ...
+    'FieldOfView', [20 5], ...
+    'MaxRange', 150, ...
+    'ActorProfiles', actorProfiles(s));
+% Use the sensor configuration to plot the sensor coverage areas. Vision
+% sensor uses the shaded blue coverage area and radar modes are shaded in
+% red.
+%for i = 1:3
+%    plotCoverageArea(cap(i), [sensorParams(i).X, sensorParams(i).Y],...
+%       sensorParams(i).Range, sensorParams(i).YawAngle, sensorParams(i).FoV);
+%end
+plotCoverageArea(cap(1), radarSensor.SensorLocation, radarSensor.MaxRange, 0, radarSensor.FieldOfView(1))
 
 %% Draw The all
 while advance(s)
