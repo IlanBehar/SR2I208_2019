251655681302274d58777369dc7660a1bc141de5
ilan <ilan.behar@telecom-paristech.fr>
2019-06-18 16:52:40 +0200

    WD

--- Attack.m
+++ Attack.m
@@ -13,9 +13,9 @@ switch num_attack
             position=[constA constB 0];
         end
     case 2
-        constA=50;
-        constB=0;
-        position=[constA+target.Position(1) constB+target.Position(2) 0];
+        A=50;
+        B=0;
+        position=[A+target.Position(1) B+target.Position(2) 0];
     case 3
         A=40-randi(80);
         B=10-randi(20);
@@ -40,7 +40,14 @@ switch num_attack
             constB=attacker.Position(2);
             position=[constA constB 0];
          end
-    %case 6 looks to be the same as case 2
+    case 6
+        A=50;
+        if t==0
+            B=line(2*randi(size(line)/2)+1);
+            position=[A+target.Position(1) B 0];
+        else
+            position=[A+target.Position(1) attacker.Position(2) 0];
+        end
     case 7
         if t==0
             constA=40-randi(80);
@@ -51,5 +58,28 @@ switch num_attack
             constB=attacker.Position(2);
             position=[constA constB 0];
         end
+    case 9
+        Vmax=30/3.6; %maximal variation
+        if(attacker.Velocity(1)<Vmax)
+            delta=1;
+        else
+            delta=(1-2*(randi(2)-1))*Vmax;
+        end
+        traj=[attacker.Position(1) attacker.Position(2); roadLength attacker.Position(2)];
+        trajectory(attacker,traj(:,:), attacker.Velocity(1)+delta);
+        position=attacker.Position;
+    case 10
+        jump=10; %distance the attacker will "jump"
+        position=[jump+attacker.Position(1) attacker.Position(2) 0];
+    case 11
+        A=50;
+        B=0;
+        angleSnap=20;
+        position=[A+target.Position(1) B+target.Position(2) 0];
+        attacker.Yaw=attacker.Yaw+angleSnap;
+    case 12
+        A=1;
+        B=1;
+        position=[A+target.Position(1) B+target.Position(2) 0];        
 end
 
