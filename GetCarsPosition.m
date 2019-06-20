1e1fa6f87d27805d5cab4f4c52c50b5cba16c6b3
ilan <ilan.behar@telecom-paristech.fr>
2019-06-20 12:11:53 +0200

    removed a useless output variable

--- GetCarsPosition.m
+++ GetCarsPosition.m
@@ -1,6 +1,5 @@
-function [output] = GetCarsPosition(carPerSecond, laneNumber, speed, roadLength)
+function [cars] = GetCarsPosition(carPerSecond, laneNumber, speed, roadLength)
 %Set the number of car on the road
 delta=roadLength*3/10;
 carsPerLanePerSecond=carPerSecond/laneNumber;
 cars=[delta/2:speed/carsPerLanePerSecond:roadLength-delta];
-output=cars;
